f95c39793827f189514ea6d9c89b9d8818e9a142
root <root@u-site.cloud>
2024-04-06 15:23:07 +0000

    upt

--- Api_fc/settings.py
+++ Api_fc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-98jek_a%5sq^dws8ze!)l6bbs8f%!wblwde)h3$z5h7lo9bpqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ia.facialcheck.co', '/run/socket_ia_fc.sock']
 
 
 # Application definition
