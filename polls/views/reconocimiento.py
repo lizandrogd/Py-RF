85a158920758e3aa15c7ea8dc5a7349413199849
lizandrogd <77708265+lizandrogd@users.noreply.github.com>
2024-05-21 00:09:14 -0500

    Update reconocimiento.py

--- polls/views/reconocimiento.py
+++ polls/views/reconocimiento.py
@@ -22,13 +22,16 @@ def reconocimiento_facial(request):
         # Convertir la imagen a escala de grises
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+        # Mejorar el contraste de la imagen
+        gray = cv2.equalizeHist(gray)
+
         # Inicializar el clasificador de rostros en cascada de Haar
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
         resultados = []
 
         # Detectar rostros en la imagen con parámetros ajustados
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(30, 30))
 
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y+h, x:x+w]
@@ -36,19 +39,20 @@ def reconocimiento_facial(request):
 
             # Aplicar preprocesamiento
             roi_gray_resized = roi_gray_resized.astype('float32') / 255.0
+            roi_gray_resized_flattened = roi_gray_resized.flatten().reshape(1, -1)
             
             # Hacer predicción con KNN
-            knn_prob = knn_clf.predict_proba(roi_gray_resized.flatten().reshape(1, -1))
+            knn_prob = knn_clf.predict_proba(roi_gray_resized_flattened)
             knn_label = knn_clf.classes_[np.argmax(knn_prob)]
             knn_confidence = np.max(knn_prob)
             
             # Hacer predicción con SVM
-            svm_prob = svm_clf.decision_function(roi_gray_resized.flatten().reshape(1, -1))
+            svm_prob = svm_clf.decision_function(roi_gray_resized_flattened)
             svm_label = svm_clf.classes_[np.argmax(svm_prob)]
             svm_confidence = np.max(svm_prob)
 
             # Verificar si las etiquetas predichas coinciden en ambos modelos y si superan un umbral de confianza
-            confidence_threshold = 0.8  # Ajustar según sea necesario
+            confidence_threshold = 0.9  # Aumentar umbral de confianza para mayor rigurosidad
             if knn_label == svm_label and knn_confidence >= confidence_threshold and svm_confidence >= confidence_threshold:
                 resultados.append(str(knn_label))
             else:
