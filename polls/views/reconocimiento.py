03a9c3aa2d69b4472ce6cd92aadd74725249909f
lizandrogd <77708265+lizandrogd@users.noreply.github.com>
2024-07-05 11:58:22 -0500

    Update reconocimiento.py

--- polls/views/reconocimiento.py
+++ polls/views/reconocimiento.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import joblib
 import face_recognition
+from polls.views.consulta import procesar_resultados
 
 # Cargar los modelos entrenados (KNN y SVM)
 knn_clf = joblib.load('modelo_knn_con_aumento_con_desconocido.pkl')
@@ -45,36 +46,14 @@ def reconocimiento_facial(request):
                         results.append("Desconocido")
                 
                 # Función para procesar los resultados (puedes ajustar según tu necesidad)
-                def procesar_resultados(resultados):
-                    # Aquí podrías realizar cualquier procesamiento adicional de los resultados
-                    return resultados
-                
-                # Llamar a la función para procesar resultados y retornar la respuesta
-                response_data = {
-                    'success': True,
-                    'message': 'Rostros detectados',
-                    'face_locations': face_locations,
-                    'results': procesar_resultados(results)
-                }
+                        return procesar_resultados(results)
+
             
             else:
-                response_data = {
-                    'success': False,
-                    'message': 'No se detectaron rostros en la imagen'
-                }
+                 return JsonResponse({"error": "No se dtectaron tostros en la imagen"})
         
-        except Exception as e:
-            response_data = {
-                'success': False,
-                'message': f'Error al procesar la imagen: {str(e)}'
-            }
-        
-        return JsonResponse(response_data)
+        except Exception as e:            
+            return JsonResponse({"error": f'Error al procesar la imagen: {str(e)}'})
     
     else:
-        # Manejar el caso en el que no se reciba una imagen
-        response_data = {
-            'success': False,
-            'message': 'No se proporcionó una imagen válida'
-        }
-        return JsonResponse(response_data)
+         return JsonResponse({"error": "Debe proporcionar una imagen en la solicitud POST."})
