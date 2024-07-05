9bc9e854a3c015a72f140e353dacdac5da7c588c
lizandrogd <77708265+lizandrogd@users.noreply.github.com>
2024-07-05 10:54:09 -0500

    Update entrenamiento.py

--- polls/views/entrenamiento.py
+++ polls/views/entrenamiento.py
@@ -19,7 +19,7 @@ def cargar_imagenes_y_etiquetas(ruta_dataset):
         for imagen_nombre in os.listdir(carpeta_persona):
             imagen_ruta = os.path.join(carpeta_persona, imagen_nombre)
             imagen = face_recognition.load_image_file(imagen_ruta, mode='RGB')
-            caras_codificadas = face_recognition.face_encodings(imagen)
+            caras_codificadas = face_recognition.face_encodings(imagen, num_jitters=1)
 
             if len(caras_codificadas) > 0:
                 imagen_codificada = caras_codificadas[0]
