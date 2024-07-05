cab463a7cf212560b009ec914d37992e79f96a81
lizandrogd <77708265+lizandrogd@users.noreply.github.com>
2024-07-05 14:05:31 -0500

    Update reconocimiento.py

--- polls/views/reconocimiento.py
+++ polls/views/reconocimiento.py
@@ -4,7 +4,7 @@ import joblib
 import cv2
 import face_recognition
 import numpy as np
-from sklearn.svm import SVC  # Import SVC from scikit-learn
+from sklearn.svm import SVC
 
 from polls.views.consulta import procesar_resultados
 
@@ -21,37 +21,50 @@ def reconocimiento_facial(request):
             
             # Load image with face_recognition
             image = face_recognition.load_image_file(image_file)
-             # Redimensionar la imagen a un tamaño específico si es necesario
-          
+            
             # Detect faces in the image
             face_locations = face_recognition.face_locations(image)
             
             # If faces are detected, process the image
             if face_locations:
-                # Extract facial encodings from detected faces
-                face_encodings = face_recognition.face_encodings(image, face_locations)
-                
                 # Initialize list to store results
                 results = []
-                
-                # Iterate over each facial encoding found
-                for face_encoding in face_encodings:
-                    # Use KNN to predict the label
-                    knn_prediction = knn_clf.predict_proba([face_encoding])
-                    knn_name = knn_clf.classes_[np.argmax(knn_prediction)]
+
+                # Process each detected face
+                for face_location in face_locations:
+                    top, right, bottom, left = face_location
+                    rostro = image[top:bottom, left:right]
                     
-                    # Use SVM to predict the label
-                    svm_scores = svm_clf.decision_function([face_encoding])
-                    svm_probabilities = np.exp(svm_scores) / np.sum(np.exp(svm_scores), axis=1, keepdims=True)
-                    svm_name = svm_clf.classes_[np.argmax(svm_probabilities)]
+                    # Redimensionar y normalizar la imagen al tamaño esperado (224x224)
+                    rostro_rgb_resized = cv2.resize(rostro, (224, 224))
+                    rostro_rgb_resized_normalized = cv2.normalize(rostro_rgb_resized, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
                     
-                    # Check if KNN and SVM predictions are equal
-                    if knn_name != svm_name:
-                        # Add predicted label only if both predictions are equal
-                        results.append(str(knn_name))
-                    else:
-                        # If predictions are not equal, add "Unknown"
-                        results.append("Desconocido")
+                    # Convert image to RGB
+                    rostro_rgb_resized_normalized_rgb = cv2.cvtColor(rostro_rgb_resized_normalized, cv2.COLOR_BGR2RGB)
+                    
+                    # Extract facial encodings
+                    face_encodings = face_recognition.face_encodings(rostro_rgb_resized_normalized_rgb)
+                    
+                    # Ensure that at least one encoding is found
+                    if face_encodings:
+                        face_encoding = face_encodings[0]
+                        
+                        # Use KNN to predict the label
+                        knn_prediction = knn_clf.predict_proba([face_encoding])
+                        knn_name = knn_clf.classes_[np.argmax(knn_prediction)]
+                        
+                        # Use SVM to predict the label
+                        svm_scores = svm_clf.decision_function([face_encoding])
+                        svm_probabilities = np.exp(svm_scores) / np.sum(np.exp(svm_scores), axis=1, keepdims=True)
+                        svm_name = svm_clf.classes_[np.argmax(svm_probabilities)]
+                        
+                        # Check if KNN and SVM predictions are equal
+                        if knn_name != svm_name:
+                            # Add predicted label only if both predictions are equal
+                            results.append(str(knn_name))
+                        else:
+                            # If predictions are not equal, add "Unknown"
+                            results.append("Desconocido")
                 
                 # Process results as needed (here using a function procesar_resultados)
                 return procesar_resultados(results)
