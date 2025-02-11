37cec90866d155bfc6c9231b9e8b84545f50ae49
lizandrogd <77708265+lizandrogd@users.noreply.github.com>
2025-02-10 19:44:48 -0500

    eliminacion db correccion de settings

--- Api_fc/settings.py
+++ Api_fc/settings.py
@@ -94,8 +94,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
-
-}
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
